519db6b22b3892b82f5dd3884e081fbc80279d06
vaneck237 <vaneckbilongo@gmail.com>
2024-09-11 16:48:26 +0100

    add policyholder translations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import messages_am_et from "./translations/am_et.json";
+import flatten from "flat";
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: "am_et", messages: messages_am_et }],
+  "translations": [{ key: "am_et", messages: flatten(messages_am_et) }],
 }
 
 export const LanguageAmEtModule = (cfg) => {
